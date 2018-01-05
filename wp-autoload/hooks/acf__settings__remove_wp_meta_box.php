41495ea9e29f6363beada944d2731faaba35a0d0
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-05 13:44:08 -0500

    Directory CSS fixes

---
+++ wp-autoload/hooks/acf__settings__remove_wp_meta_box.php
@@ -0,0 +1,3 @@
+<?php
+// Advanced Custom Fields 5.6.1 removes built-in Custom Fields from post/page screen options.
+add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
--- wp-autoload/hooks/the_content.php
+++ wp-autoload/hooks/the_content.php
@@ -5,3 +5,23 @@ add_filter(
 		return str_replace( '<p></p>', '', $content );
 	}
 );
+
+/**
+ * Modify the legacy classes on the directory page. Probably not very good.
+ */
+add_filter( 'the_content', function( $content ) {
+	global $post;
+
+	if ( 'Staff' !== $post->post_title ) {
+		return $content;
+	}
+
+	$content = str_replace( 'headerline', 'primary row', $content );
+	$content = str_replace( 'li class="clearfix', 'li class="clearfix row directory-row', $content );
+
+	for ( $i = 1; $i <= 12; $i += 1 ) {
+		$content = str_replace( "span$i", "col-12 col-md-$i", $content );
+	}
+
+	return $content;
+} );
