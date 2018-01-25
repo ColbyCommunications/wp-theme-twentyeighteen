19bc96dc0c3763d3498fc6cf118ae49ad49e5480
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-25 15:34:07 -0500

    Add github protocols config"

--- taxonomy-schedule_category.php
+++ taxonomy-schedule_category.php
@@ -5,6 +5,7 @@
  * @package colbycomms/wp-theme-twentyeighteen
  */
 
+use Carbon_Fields\Helper\Helper as Carbon;
 use ColbyComms\Schedules\Shortcodes\ScheduleShortcode;
 use ColbyComms\Schedules\Utils\WpQuery;
 use ColbyComms\TwentyEighteen\{PageHeader, TwentyEighteen as T18};
@@ -23,12 +24,16 @@ $queried_object = get_term_by( 'slug', get_query_var( 'schedule_category' ), 'sc
 	<?php T18::schedule_archive_header( $queried_object ); ?>
 	<div class="container-lg mx-auto mb-5">
 		<div class="mb-3">
-		<?php echo $queried_object->description; ?>
+		<?php echo apply_filters( 'the_content', $queried_object->description ); ?>
 		</div>
 		<?php
+		$do_tag_selector = Carbon::get_term_meta( $queried_object->term_id, 'colby_schedule__do_tag_list' ) ? 'true' : 'false';
+		$do_description = Carbon::get_term_meta( $queried_object->term_id, 'colby_schedule__hide_description' ) ? 'false' : 'true';
 		echo ScheduleShortcode::render(
 			new WpQuery( $wp_query ),
-			$queried_object->parent ? ['active' => 'Signature Events' ] : [],
+			$queried_object->parent
+				? [ 'active' => 'Signature Events', 'tag-selector' => $do_tag_selector, 'show-description' => $do_description ]
+				: [ 'tag-selector' => $do_tag_selector, 'show-description' => $do_description ],
 			$queried_object->parent ? $queried_object : null
 		);
 		?>
